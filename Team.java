148a1435a6a924b503233e62c3c2c2b19107475b
Billvace <1847069979@qq.com>
2018-11-02 09:28:51 +0800

    Draft 2

--- Team.java
+++ Team.java
@@ -13,9 +13,21 @@ public class Team {
         InputOutput.output("Welcome to the TEAM Building system !!!!!");
         InputOutput.output("========================");
         
+        int members = InputOutput.inputInt("Memebers in the team: ");
+        Member[] team = new Member[members];
         
+        for (int i = 0; i< members; i++) {
+        	team[i] = addMembers();
+        }
         
     }
-    }
-	
+		public Member addMembers() {
+			String Info = InputOutput.input("Name:Surname:Sex:DD/MM/YYYY");
+			String[] Info_input = Info.split("[:/]");
+			NewMember = Member(Info_input[0],Info_input[1],Info_input[2].toCharArray()[0],(Info_input[3]),int(Info_input[4]),int(Info_input[5]));
+			
+			return  NewMember;
+		}
 }
+	
+
